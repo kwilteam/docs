f6fb4e8f0d37a675b0ccff66f779a4fdc253c575
KwilLuke <105600466+KwilLuke@users.noreply.github.com>
2023-10-17 14:15:32 -0500

    minor fix for edit pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ module.exports = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kwilteam/docs/tree/main/',
+            'https://github.com/kwilteam/docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
