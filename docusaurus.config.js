c28d84259c92458c9b022e7693b962f4ec40aab2
Luke Lamey <105600466+KwilLuke@users.noreply.github.com>
2024-05-07 12:18:53 -0500

    And short docs for GO SDK (#100)
    * And short docs for GO SDK
    
    We have had a couple teams ask about the Go SDK docs, so adding this reference here so they will be more discoverable
    
    * address jon comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,6 @@ module.exports = {
     // ... Your other themes.
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import('@easyops-cn/docusaurus-search-local').PluginOptions} */
       ({
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
